69faad561c4294a523e0816a4148122fa0a36b7f
Yaroslav <kznkv.test@gmail.com>
2023-05-25 20:06:14 +0300

    added Logo and Menu

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -1,7 +1,35 @@
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Container } from '@mui/material'
+import Logo from 'components/Logo/Logo'
+import Menu from 'components/Menu/Menu'
+
 import './Header.scss'
 
 type Props = {}
+
 const Header = (props: Props) => {
-    return <div className="red">Header</div>
+    return (
+        <AppBar position="static">
+            <Container>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Logo />
+                    <Menu />
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
 }
+
 export default Header
