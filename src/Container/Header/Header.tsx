b93e4c4796a749465ead0787f37427f90127dd3f
Yaroslav <kznkv.test@gmail.com>
2023-05-25 20:38:16 +0300

    added StyledEngine and CartHeader

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -7,12 +7,13 @@ import Logo from 'components/Logo/Logo'
 import Menu from 'components/Menu/Menu'
 
 import './Header.scss'
+import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
-        <AppBar position="static">
+        <AppBar position="static" className="app-bar">
             <Container>
                 <Toolbar>
                     <IconButton
@@ -26,6 +27,7 @@ const Header = (props: Props) => {
                     </IconButton>
                     <Logo />
                     <Menu />
+                    <CartHeader />
                 </Toolbar>
             </Container>
         </AppBar>
